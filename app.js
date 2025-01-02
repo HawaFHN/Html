e715f6ed9d493746077da58a3e4f421d93aea64c
Hawanur <hawanur164@gamil.com>
2025-01-02 15:24:54 +0600

    fourth commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let mail       = document.getElementById('email')
+let password   = document.getElementById('Password')
+
+let login = ()=>{
+    mail        = mail.value  
+    password    = password.value
+   if(mail=='akhi.web@gmail.com' && password =="12345"){
+    console.log("login done");
+       alert("login done")
+
+   }
+   else{
+    console.log("sorry");
+    alert("sorry !")
+    
+
+   }
+}
