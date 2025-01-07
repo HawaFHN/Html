1dfc4508b3292816b35c904cda478b185c142027
Hawanur <hawanur164@gamil.com>
2025-01-07 15:30:22 +0600

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let mail       = document.getElementById('email')
 let password   = document.getElementById('password')
+let food       =document.getElementById('food')
+let login_sec      =document.getElementById('login_sec')
 
 let login = ()=>{
     mail        = mail.value  
     password    = password.value
-   if(mail=='akhi.web@gmail.com' && password =="12345"){
+   if(mail=='hawanur.web@gmail.com' && password =="12345"){
     console.log("login done");
-       alert("login done")
+    login_sec.classList.add('hidden')
+       food.classList.remove('hidden')
+
 
    }
    else{
